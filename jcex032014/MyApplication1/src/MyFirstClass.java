a28a2e7c97fd6f9ad29845327236dccaade29457
unknown <lamaniobra@gmail.com>
2014-10-21 00:29:03 +0300

    modified:   jcex032014/MyApplication1/src/MyFirstClass.java

--- jcex032014/MyApplication1/src/MyFirstClass.java
+++ jcex032014/MyApplication1/src/MyFirstClass.java
@@ -1,5 +1,8 @@
 public class MyFirstClass {
-    public static void main(String[] args){
+    public static void main(String[] args){ 
+        
+         
+        
         System.out.println("Hello");
     }
 }
